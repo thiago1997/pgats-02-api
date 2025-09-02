7c3d5d2becc2e72374e442019217b1c5438a468c
Thiago Neves <thiago.snevez@hotmail.com>
2025-09-02 19:48:23 -0300

    Habilitando a pipeline no Github Actions 3.0

---
+++ test/controller/transferController.teste.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const app = require('../../app');
+
+describe ('Transfer Controller', () => {
+    describe ('POST /transfer', () => {
+        it('Quando informo remetente e destinatario inexistentes recebo 400', async () => {
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                      from: "thiago1",
+                      to: "thiago",
+                      amount: 2000
+                }); 
+            expect(resposta.status).to.equal(400); 
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado')
+        });
+    });
+
+    describe ('GET /transfers', () => {
+ 
+    });
+});
